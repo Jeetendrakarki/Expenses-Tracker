4ba9496841ac709970bb9b632a8635dff0728177
Jitendra Karki <jitendrakarki921@gmail.com>
2023-03-21 19:10:41 +1100

    app done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
-import {Button} from "react-bootstrap";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Registration } from "./pages/Registration";
+import { ForgetPassword } from "./pages/ForgetPassword";
+import { Dashboard } from "./pages/Dashboard";
+import { ToastContainer } from "react-toastify";
+import { PrivateRoute } from "./pages/PrivateRoute";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    fName: "Prem",
+  });
   return (
-    <div>
-<button>test button</button>      
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="register" element={<Registration />} />
+          <Route path="password-reset" element={<ForgetPassword />} />
+
+          <Route
+            path="dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
     </div>
   );
 }
